ff05d8e0ec7136de89d74ff966c72e438739826f
Francesco Dorati <fd.francescodorati@gmail.com>
2023-03-29 00:39:39 +0200

    Initial Commit

---
+++ MiniGames/MiniGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniGamesApp.swift
+//  MiniGames
+//
+//  Created by Francesco Dorati on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
